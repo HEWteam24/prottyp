daa6314d57eee8151ef039a73493e8f0360a875e
tsuyoshitsu <117137895+tsuyoshitsu@users.noreply.github.com>
2022-11-01 13:39:09 +0900

    Add files via upload

---
+++ PROT_1101/bullet.cpp
@@ -0,0 +1,130 @@
+#include "bullet.h"
+#include "enemy.h"
+#include "texture.h"
+#include "sprite.h"
+
+//================================
+//グローバル変数
+//================================
+BULLET g_Bullet[BULLET_MAX];
+
+
+//================================
+//初期化
+//================================
+
+HRESULT InitBullet()
+{
+	//テクスチャのロード
+	int texno = LoadTexture((char*)"data\\TEXTURE\\bullet_P.png");
+	//構造体の初期化
+	for (int i = 0; i < BULLET_MAX; i++)
+	{
+		g_Bullet[i].use = false;
+		g_Bullet[i].w = BULLET_SIZE_W;
+		g_Bullet[i].h = BULLET_SIZE_H;
+		g_Bullet[i].pos = D3DXVECTOR2(0, 0);
+		g_Bullet[i].texNo = texno;
+		g_Bullet[i].mov = D3DXVECTOR2(0, -BULLET_SPEED);
+
+	}
+	return S_OK;
+}
+
+//終了処理
+void UninitBullet()
+{
+}
+
+//更新処理
+void UpdateBullet()
+{
+	ENEMY* pEnemy = GetEnemy();
+
+	for (int i = 0; i < BULLET_MAX; i++)
+	{
+		if (g_Bullet[i].use == true)
+		{
+			g_Bullet[i].oldpos = g_Bullet[i].pos;	//当たり判定用の座標コピー
+
+			g_Bullet[i].pos += g_Bullet[i].mov;	//移動
+
+			if (g_Bullet[i].pos.y < 0 - g_Bullet[i].h / 2)
+			{
+				g_Bullet[i].use = false;
+			}
+
+			if (pEnemy->use)
+			{
+				if (
+					g_Bullet[i].pos.y - (g_Bullet[i].h / 2) < (pEnemy->pos.y + pEnemy->size.y / 2) &&
+					g_Bullet[i].oldpos.y - (g_Bullet[i].h / 2) > (pEnemy->pos.y - pEnemy->size.y / 2) &&
+					g_Bullet[i].pos.x < (pEnemy->pos.x + pEnemy->size.x / 2) &&
+					g_Bullet[i].pos.x >(pEnemy->pos.x - pEnemy->size.x / 2)
+					)
+				{
+					pEnemy->hp -= 100;
+					g_Bullet[i].use = false;
+
+					//if (pEnemy->hp <= 0)
+					//{
+					//	pEnemy->use = false;
+					//}
+				}
+
+			}
+		}
+
+
+	}
+}
+
+void DrawBullet()
+{
+	
+	for (int i = 0; i < BULLET_MAX; i++)
+	{
+		if (g_Bullet[i].use)
+		{
+			////テクスチャのセット
+			//GetDeviceContext()->PSSetShaderResources
+			//(0, 1, GetTexture(g_Bullet[i].texNo));	//スプライトの描画
+
+			//DrawSprite
+			//(
+			//	g_Bullet[i].pos.x,
+			//	g_Bullet[i].pos.y,
+			//	g_Bullet[i].w,
+			//	g_Bullet[i].h,
+			//	g_Bullet[i].rot,
+			//	D3DXCOLOR(1.0, 1.0, 1.0, 1.0),	//白色
+			//	g_Bullet[i].texNo,		//アニメーションパターン番号
+			//	1,				//パターンの横サイズ
+			//	1				//    　　　縦サイズ
+			//						//横に並ぶパターンの数
+
+			//);
+
+			DrawSpriteColor(g_Bullet[i].texNo, g_Bullet[i].pos.x, g_Bullet[i].pos.y, BULLET_SIZE_W, BULLET_SIZE_H,
+				0.0f, 0.0f, 1.0f, 1.0f, D3DXCOLOR(1.0, 1.0, 1.0, 1.0));
+		}
+	}
+}
+
+BULLET * GetBullet()
+{
+	return &g_Bullet[0];
+}
+
+void SetBullet(D3DXVECTOR2 pos)
+{
+	for (int i = 0; i < BULLET_MAX; i++)
+	{
+		if (g_Bullet[i].use == false)
+		{
+			g_Bullet[i].pos = pos;
+			g_Bullet[i].use = true;
+			return;
+		}
+	}
+}
---
+++ PROT_1101/bullet.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "main.h"
+#include "renderer.h"
+
+//==================================
+//マクロ定義
+//==================================
+#define	BULLET_MAX		12		//弾の最大数
+#define	BULLET_SPEED	13.0f	//弾のスピード
+#define	BULLET_SIZE_W	50		//弾の横サイズ
+#define	BULLET_SIZE_H	50		//弾の縦サイズ
+
+//構造体
+struct BULLET
+{
+	bool use;
+	float w, h;
+	D3DXVECTOR2	pos;
+	D3DXVECTOR2	oldpos;	//当たり判定用
+	float	rot;
+	D3DXVECTOR2	mov;	//移動ベクトル
+	int	texNo;
+};
+
+//==================================
+//プロトタイプ宣言
+//==================================
+HRESULT InitBullet();
+void	UninitBullet();
+void	UpdateBullet();
+void	DrawBullet();
+
+BULLET*	GetBullet();
+void	SetBullet(D3DXVECTOR2 pos);	//弾発生
