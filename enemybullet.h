155e64ff3fb9c040f9bcffe89d307653a00c4e15
myz <matchy1518@gmail.com>
2022-11-11 23:21:28 +0900

    敵の弾のマップチップができたお！ 現状5*90のチップのみだから90秒の曲のみに対応しています。 別のサイズのマクロ定義および、変数を同様に作成すれば使用できます。
    敵の弾のダメージを少し変更しました。詳細は変更差分を参照してください。

--- enemybullet.h
+++ enemybullet.h
@@ -13,6 +13,11 @@
 #define	ENEMYBULLET_SIZE_W	140		//弾の横サイズ
 #define	ENEMYBULLET_SIZE_H	60		//弾の縦サイズ
 
+//以下マップチップ用
+#define	MAP_SIZE_X	5	//横レーン数
+#define	MAP_SIZE_Y	90	//縦ノーツ総数(現状１秒に１個)
+
+
 //==================================
 //グローバル変数
 //==================================
@@ -36,6 +41,9 @@ void	UninitEnemyBullet();
 void	UpdateEnemyBullet();
 void	DrawEnemyBullet();
 
+
+
 ENEMYBULLET* GetEnemyBulletNomal();
 ENEMYBULLET* GetEnemyBulletLong();
 void	SetEnemyBullet();	//弾発生
+void	SETBULLET();
